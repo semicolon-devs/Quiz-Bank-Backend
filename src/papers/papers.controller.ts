8ceda88e35b13f10e40d20eaf6ed1559e045ad48
Lasind Anjana <lasindua@gmail.com>
2023-12-10 01:56:26 +0530

    enable authentication

--- src/papers/papers.controller.ts
+++ src/papers/papers.controller.ts
@@ -4,24 +4,31 @@ import {
   Delete,
   Get,
   Param,
-  ParseIntPipe,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { PapersService } from './papers.service';
 import { CreatePaperDto } from './dto/create-paper.dto';
 import { ParseObjectIdPipe } from 'src/utils/validation/parseObjectIDPipe';
 import { ObjectId } from 'mongoose';
 import { AddQuestionsDto } from './dto/add-questions.dto';
+import { Roles } from 'src/auth/decorator/roles.decorator';
+import { Role } from 'src/enums/roles.enum';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('api/v1/papers')
 export class PapersController {
   constructor(private readonly papersService: PapersService) {}
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(Role.ADMIN, Role.MODERATOR)
   @Post()
   create(@Body() createPaperDto: CreatePaperDto) {
     return this.papersService.create(createPaperDto);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(Role.ADMIN, Role.MODERATOR)
   @Post('add/:id')
   addQuestions(
     @Param('id', ParseObjectIdPipe) paper_id: ObjectId,
@@ -30,16 +37,22 @@ export class PapersController {
     return this.papersService.addQuestion(paper_id, addQuestionsDto);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(Role.ADMIN, Role.MODERATOR, Role.USER)
   @Get()
   findAll() {
     return this.papersService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(Role.ADMIN, Role.MODERATOR, Role.USER)
   @Get(':id')
   findOne(@Param('id', ParseObjectIdPipe) id:ObjectId) {
     return this.papersService.findOne(id);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(Role.ADMIN, Role.MODERATOR, Role.USER)
   @Get(':paper_id/:question_index')
   getQuestion(
     @Param('paper_id', ParseObjectIdPipe) paperId: ObjectId,
@@ -48,6 +61,8 @@ export class PapersController {
     return this.papersService.findQuestion(paperId, question_index);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(Role.ADMIN, Role.MODERATOR, Role.USER)
   @Get('answer/:paper_id/:question_index')
   getAnswer(
     @Param('paper_id', ParseObjectIdPipe) paperId: ObjectId,
@@ -56,6 +71,8 @@ export class PapersController {
     return this.papersService.findAnswer(paperId, question_index);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(Role.ADMIN, Role.MODERATOR)
   @Delete(':paper_id/:question_index')
   removeQuestion(
     @Param('paper_id', ParseObjectIdPipe) paperId: ObjectId,
--- src/questions/questions.controller.ts
+++ src/questions/questions.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Delete,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { QuestionsService } from './questions.service';
 import { CreateQuestionDto } from './dto/create-question.dto';
@@ -14,31 +15,44 @@ import { UpdateQuestionDto } from './dto/update-question.dto';
 import { ParseObjectIdPipe } from 'src/utils/validation/parseObjectIDPipe';
 import { ObjectId } from 'mongoose';
 import { FilterDTO } from './dto/filter.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { Roles } from 'src/auth/decorator/roles.decorator';
+import { Role } from 'src/enums/roles.enum';
 
 @Controller('api/v1/questions')
 export class QuestionsController {
   constructor(private readonly questionsService: QuestionsService) {}
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(Role.ADMIN, Role.MODERATOR)
   @Post()
   create(@Body() createQuestionDto: CreateQuestionDto) {
     return this.questionsService.create(createQuestionDto);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(Role.ADMIN, Role.MODERATOR, Role.USER)
   @Get()
   findAll() {
     return this.questionsService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(Role.ADMIN, Role.MODERATOR, Role.USER)
   @Get('filter')
   filter(@Query() allQueryParams: FilterDTO) {
     return this.questionsService.filter(allQueryParams);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(Role.ADMIN, Role.MODERATOR, Role.USER)
   @Get(':id')
   findOne(@Param('id', ParseObjectIdPipe) id: ObjectId) {
     return this.questionsService.findOne(id);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(Role.ADMIN, Role.MODERATOR)
   @Patch(':id')
   update(
     @Param('id', ParseObjectIdPipe) id: ObjectId,
@@ -47,6 +61,8 @@ export class QuestionsController {
     return this.questionsService.update(id, updateQuestionDto);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(Role.ADMIN, Role.MODERATOR)
   @Delete(':id')
   remove(@Param('id', ParseObjectIdPipe) id: ObjectId) {
     return this.questionsService.remove(id);

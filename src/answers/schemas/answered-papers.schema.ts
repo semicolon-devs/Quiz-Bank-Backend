16eeed2a41fc1c80fb800e5a2102e719b9bcee96
SupulCFG <supuledirisinghe@gmail.com>
2023-12-18 03:41:21 +0530

    added answer_endpoints

---
+++ src/answers/schemas/answered-papers.schema.ts
@@ -0,0 +1,17 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+import { Attempt } from "./attempts.schema";
+
+export type AnsweredPaperDocument = HydratedDocument<AnsweredPaper>;
+
+@Schema({ timestamps: true})
+export class AnsweredPaper {
+    @Prop({type: String, required: true})
+    userId: string;
+
+    @Prop({type: Attempt}) // required??
+    attempts: Attempt[];
+
+}
+
+export const AnsweredPaperSchema = SchemaFactory.createForClass(AnsweredPaper);

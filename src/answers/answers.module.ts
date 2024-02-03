e09e586150eba7949a4b5dfdf34d8227b9f1ba40
SupulCFG <supuledirisinghe@gmail.com>
2024-02-03 15:23:29 +0530

    removed unused imports @answers.module

--- src/answers/answers.module.ts
+++ src/answers/answers.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AnswersController } from './answers.controller';
 import { AnswersService } from './answers.service';
-import { MongooseModule } from '@nestjs/mongoose';
 import { AnsweredPaper, AnsweredPaperSchema } from './schemas/answered-papers.schema';
-import { PapersService } from 'src/papers/papers.service';
 import { Paper, PaperSchema } from 'src/papers/schemas/paper.schema';
 import { Question, QuestionSchema } from 'src/questions/schemas/question.schema';
-import { Answered, AnsweredQuestionSchema } from './schemas/answered.schema';
 import { PapersModule } from 'src/papers/papers.module';
 
 @Module({
@@ -15,7 +13,6 @@ import { PapersModule } from 'src/papers/papers.module';
       { name: AnsweredPaper.name, schema: AnsweredPaperSchema},
       { name: Paper.name, schema: PaperSchema },
       { name: Question.name, schema: QuestionSchema, },
-      // { name: Answered.name, schema: AnsweredQuestionSchema, },
     ]),
 
     PapersModule
